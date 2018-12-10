eadca1ff2525df89ec148fa3dfaf083a2fb94f14
Jungpil YU <jungpilyu@users.noreply.github.com>
2018-12-11 00:09:12 +0900

    [feature] Complete the first 'describe'.
    Add two 'it' statements.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,25 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('has a URL defined and is not empty', function() {
+           allFeeds.forEach(function(aFeed) {
+             expect(aFeed.url).toBeDefined();
+             expect(aFeed.url.length).not.toBe(0);
+           })
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has a name defined and is not empty', function() {
+           allFeeds.forEach(function(aFeed) {
+             expect(aFeed.name).toBeDefined();
+             expect(aFeed.name.length).not.toBe(0);
+           })
+         });
+
     });
 
 
